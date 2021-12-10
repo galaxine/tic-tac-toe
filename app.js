55383d9652a6448e9070040e33fdfaa40ab372d9
galaxine <oezdemir50@gmail.com>
2021-12-10 10:58:44 +0100

    time to push it up to github for my workstation

--- app.js
+++ app.js
@@ -19,11 +19,16 @@
 
 const playerFactory = (name) => {
     let _points = 0;
+    let _identity;
+    const setIdentity = (identity) =>{
+        _identity = identity;
+    }
+    const getIdentity = () => _identity;
     const getPoints = () => _points;
     const gainPoints = () => {
         _points++;
     }
-    return {getPoints, gainPoints};
+    return {getPoints, gainPoints, setIdentity, getIdentity};
 }
 /**
  * The gamefield shows us the tic-tac-toe field
@@ -34,7 +39,41 @@ const playerFactory = (name) => {
  * not allow to change the size of the field
  */
 const gameField =(() => {
-    let ticTacToeField = [["","",""],["","",""],["","",""]]
-    return {ticTacToeField};
+    let ticTacToeField = [["A","B","C"],["D","E","F"],["G","H","I"]]
+    function getTicTacToeField() {
+        return ticTacToeField;
+    }
+    function setTicTacToeField(aTicTacToeField) {
+        ticTacToeField = aTicTacToeField;
+    }
+    return {getTicTacToeField, setTicTacToeField};
     
 })();
+
+/**
+ * Logic, should probably get two players and a gamefield passed to determine what happens
+ */
+const gameLogic =(()=> {
+    let game = gameField();
+    function testField() {
+        game.getTicTacToeField().forEach(element => {
+            element.forEach( el => {
+                console.log(el);
+            })
+        });
+    }
+    return {testField};
+})();
+
+/**
+ * Then we need to make sure it displays it on the web.
+ * Then we need to make sure it catches the interaction from the web.
+ * + We need a function that catches the elements from the dom
+ * + We need a function that listents to the users touch
+ * + 
+ * + We need a function that 
+ * 
+ */
+const DomControl = (() => {
+
+})()
