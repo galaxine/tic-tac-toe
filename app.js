812a5509db5ffbd0fe521f28093284f70a845ce1
galaxine <oezdemir50@gmail.com>
2021-12-23 15:04:27 +0100

    I finished the main menu, gamefield, stuck at gameover screen. There was something about CSS

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
 const playerFactory = (name) => {
     let _points = 0;
     let _identity;
-    const setIdentity = (identity) =>{
+    const setIdentity = (identity) => {
         _identity = identity;
     }
     const getIdentity = () => _identity;
@@ -28,7 +28,7 @@ const playerFactory = (name) => {
     const gainPoints = () => {
         _points++;
     }
-    return {getPoints, gainPoints, setIdentity, getIdentity};
+    return { getPoints, gainPoints, setIdentity, getIdentity };
 }
 /**
  * The gamefield shows us the tic-tac-toe field
@@ -38,42 +38,133 @@ const playerFactory = (name) => {
  * allow setting the field
  * not allow to change the size of the field
  */
-const gameField =(() => {
-    let ticTacToeField = [["A","B","C"],["D","E","F"],["G","H","I"]]
+const gameField = (() => {
+    let ticTacToeField = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]
     function getTicTacToeField() {
         return ticTacToeField;
     }
     function setTicTacToeField(aTicTacToeField) {
         ticTacToeField = aTicTacToeField;
     }
-    return {getTicTacToeField, setTicTacToeField};
-    
+    return { getTicTacToeField, setTicTacToeField };
+
 })();
 
 /**
  * Logic, should probably get two players and a gamefield passed to determine what happens
  */
-const gameLogic =(()=> {
-    let game = gameField();
+const gameLogic = (() => {
+    let game = gameField;
     function testField() {
         game.getTicTacToeField().forEach(element => {
-            element.forEach( el => {
+            element.forEach(el => {
                 console.log(el);
             })
         });
     }
-    return {testField};
+    return { testField };
 })();
 
 /**
- * Then we need to make sure it displays it on the web.
- * Then we need to make sure it catches the interaction from the web.
- * + We need a function that catches the elements from the dom
- * + We need a function that listents to the users touch
- * + 
- * + We need a function that 
  * 
  */
-const DomControl = (() => {
+const MainDomControl = (() => {
+    //the main container this all runs on.
 
-})()
+    const getMainMenu = () => {
+        let main = document.getElementsByClassName("main")[0];
+        /*
+        menu-items are the same, can we do that? assign the same one? I think not
+        */
+        let menu = document.createElement("div");
+        let menuItem1 = document.createElement("div")
+        let menuTitle = document.createElement("div");
+        let head = document.createElement("h1");
+
+        let buttons = document.createElement("div");
+        let menuItem2 = document.createElement("div");
+        let menuBtn1 = document.createElement("div");
+        let button1 = document.createElement("button");
+
+        let menuItem3 = document.createElement("div");
+        let menuBtn2 = document.createElement("div");
+        let button2 = document.createElement("button");
+        //add classes and populate the elements 
+        menu.classList.add("menu");
+        menuItem1.classList.add("menu-item");
+        menuTitle.classList.add("menu-title");
+        head.textContent = "Menu";
+
+        buttons.classList.add("buttons");
+        menuItem2.classList.add("menu-item");
+        menuBtn1.classList.add("menu-button");
+        button1.textContent = "One Player";
+
+        menuItem3.classList.add("menu-item");
+        menuBtn2.classList.add("menu-button");
+        button2.textContent = "Two Players";
+        //append the elements together
+        menu.append(menuItem1, buttons);
+        menuItem1.append(menuTitle);
+        menuTitle.append(head);
+        buttons.append(menuItem2, menuItem3);
+        menuItem2.append(menuBtn1);
+        menuBtn1.append(button1);
+        menuItem3.append(menuBtn2);
+        menuBtn2.append(button2);
+        main.appendChild(menu)
+    }
+    //get Game field and then add th
+    const getGameField = () => {
+        let main = document.getElementsByClassName("main")[0];
+        
+        let gridMenu = document.createElement("div");
+        gridMenu.classList.add("gridmenu");
+        //god, if I actually did this manually, I would have shot myself :)
+        for (let index = 0; index < 9; index++) {
+            let gridItem = document.createElement("div");
+            gridItem.classList.add("gridbox");
+            gridItem.dataset.nr = index;
+            gridMenu.appendChild(gridItem);
+        }
+        main.appendChild(gridMenu);
+    }
+    //GameOver, easy access
+    const getGameOver = () => {
+        let GameOver = {
+            g: "./img/G.png",
+            a: "./img/A.png",
+            m: "./img/M.png",
+            e: "./img/E.png",
+            o: "./img/Ov.png",
+            v: "./img/V.png",
+            r: "./img/R.png"
+        }
+        let main = document.getElementsByClassName("main")[0];
+        // can't I just use the getGameField function and then re-arrange the letters by the data attribute with a switch case?
+        let gridMenu = document.createElement("div");
+        gridMenu.classList.add("gridmenu");
+
+        for (let index = 0; index < 9; index++) {
+            let gridItem = document.createElement("div");
+            gridItem.classList.add("gridboxGE");
+            gridItem.dataset.nr = index;
+            switch (index) {
+                case 1:
+                    gridItem.
+                    break;
+            
+                default:
+                    break;
+            }
+
+            gridMenu.appendChild(gridItem);
+        }
+
+    }
+    return { getMainMenu, getGameField };
+
+
+})();
+
+MainDomControl.getGameField();
